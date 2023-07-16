575c13f68879f64c755ab7bfe557405320530faf
HybridChild <esbendn@tutanota.com>
2023-07-16 11:54:23 +0200

    Update CMakeLists.txt and main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
-#include "gtest/gtest.h"
+#include "gmock/gmock.h"
 
 int main(int argc, char** argv) {
-	testing::InitGoogleTest(&argc, argv);
+   testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
 }
-
